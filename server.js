3fa05c529503c776472f75ab92116dc8d586cd11
Bernardo <bernamariano@gmail.com>
2016-10-07 15:00:32 -0300

    use port from env

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 
 const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
     next()
 }
 
 app.use(allowCrossDomain)
 app.use(express.static('.'))
 
-app.listen(1338);
+app.listen(process.env.PORT || 1338)
