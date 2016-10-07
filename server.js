473b21266ee20a3fab8ab01dd6bc4190e875cfc1
Bernardo <bernamariano@gmail.com>
2016-10-07 14:58:04 -0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+}
+
+app.use(allowCrossDomain)
 app.use(express.static('.'))
 
 app.listen(1338);
